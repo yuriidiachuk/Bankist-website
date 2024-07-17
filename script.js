a3e6e2dc72063ec38c7e3d1345df74fb6f23d36b
Yurii Diachuk <yurdia817@gmail.com>
2024-07-17 10:50:51 +0200

    added tabbed component

--- script.js
+++ script.js
@@ -50,3 +50,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContant = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContant.forEach(c => c.classList.remove('operations__content--active'));
+  // Activate tabs
+  clicked.classList.add('operations__tab--active');
+  // Activate content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
