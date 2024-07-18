6ff6064138404a5aa3180d475ce1bc690211efc0
Yurii Diachuk <yurdia817@gmail.com>
2024-07-18 12:03:54 +0200

    added sticky navigation

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ tabsContainer.addEventListener('click', function (e) {
 
 // Menu fade animation
 
-const handleHover = function (e, opacity) {
+const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
@@ -90,3 +90,19 @@ const handleHover = function (e, opacity) {
 };
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
